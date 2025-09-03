6d6a1e895075700c7d1b04e179c5a01847d51179
Maryam <mamtazmaryam035@gmail.com>
2025-09-03 23:34:52 +0500

    make changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,38 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>
-  <Image
-    className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-    src="/img.jpg"
-    alt=""
-    width={384}
-    height={512}
-  />
-  <Image
-    className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-    src="/img.jpg"
-    alt=""
-    width={384}
-    height={512}
-  />
-<figure className="pt-6 md:p-8 text-center md:text-left space-y-4">
-  <blockquote>
-    <p className="text-lg font-medium">
-      “Tailwind CSS is the only framework that I&apos;ve seen scale
-      on large teams. It’s easy to customize, adapts to any design,
-      and the build size is tiny.”
-    </p>
-  </blockquote>
-  <figcaption className="font-medium">
-    <div className="text-sky-500 dark:text-sky-400">
-      maryam
-    </div>
-    <div className="text-slate-700 dark:text-slate-500">
-      PAIB students
-    </div>
-  </figcaption>
-</figure>
-    </div>
-  )
-}
+    <div className="text-4xl text-center">
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        {/* ✅ Using Next.js <Image /> instead of <img> */}
+        <Image
+          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+          src="/modern-ecommerce-interface.png"
+          alt="Ecommerce Interface"
+          width={384}
+          height={512}
+          priority
+        />
 
-export default page
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            {/* ✅ Escaped quotes so no ESLint error */}
+            <p className="text-lg font-medium">
+              &quot;Making my Next.js project.&quot;
+            </p>
+          </blockquote>
+
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">Ayesha Irfan</div>
+            <div className="text-slate-700 dark:text-slate-500">
+              PIAB, Student
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+    </div>
+  );
+};
+
+export default Page;
